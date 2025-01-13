fcfbab63b582e2042e1c1ac554ff756cebb783c9
Jacob Salois <hj391947@ens.uqam.ca>
2025-01-13 10:10:42 -0500

    Feat(game): Implements game_create

--- src/game.c
+++ src/game.c
@@ -1,9 +1,35 @@
+#include <stdio.h>
+#include <stdib.h>
 #include "../include/game.h"
+#include "../include/constants.h"
+
+
 
 struct game* game_create(int width, int height, int players_nb, 
         int bots_nb, SDL_Renderer* renderer) {
-    return NULL;
+    struct game* game = malloc(sizeof(struct game));
+    if(game == NULL) {
+        printf(ERR_MSG_ALLOC, "game struct");
+        return NULL;
+    }
+    if(renderer == NULL) {
+        printf("game_create : Rederer is null, unable to create game\n");
+        return NULL;
+    }
+    game->grid = init_grid(game);
+    if(game->grid != NULL) {
+        game->state = GAME_STATE_PLAY;
+        game->player_turn = PINK;
+        game->width = width;
+        game->height = height;
+        game->players_nb = players_nb;
+        game->bots_nb = bots_nb;
+        game->renderer = renderer;
+    }
 }
+
+
+
 void game_run(struct game* game) {
 
 }
